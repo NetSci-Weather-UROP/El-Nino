b5223bdc7dae4988c5ba04880b242ad2e60a5c00
Gergely Rozgonyi <97564777+gRozgonyi@users.noreply.github.com>
2022-07-07 19:07:15 +0100

    added function to get actual date for NCEP

--- nino.jl
+++ nino.jl
@@ -1,1 +1,11 @@
 #!/usr/bin/env julia
+
+using HDF5, Dates
+
+"""
+Convert NCEP dates to the actual date.
+"""
+function convert_time(time)
+    basetime = DateTime(1800,1,1)
+    time = Dates.Hour.(time) .+ basetime
+end

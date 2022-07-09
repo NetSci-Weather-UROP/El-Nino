093db7b594cbe33bdb75abc2d5d5f220f37d17f2
Abhinav Praveen <abhinav@praveen.org.uk>
2022-07-09 05:26:57 +0100

    read_air_data convenience function

--- nino.jl
+++ nino.jl
@@ -9,3 +9,7 @@ function convert_time(time)
     basetime = DateTime(1800,1,1)
     time = Dates.Hour.(time) .+ basetime
 end
+
+function read_air_data(year)
+    return h5open("air.sig995/air.sig995.$year.nc")
+end

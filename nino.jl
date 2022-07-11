f4ce3d0261450a9b12a676b7ea5a82ba4b855f0c
Abhinav Praveen <abhinav@praveen.org.uk>
2022-07-11 05:29:41 +0100

    Find inside region function.

--- nino.jl
+++ nino.jl
@@ -13,3 +13,44 @@ end
 function read_air_data(year)
     return h5open("air.sig995/air.sig995.$year.nc")
 end
+
+struct pos_data # Positional data
+    data::Matrix
+    lat::Vector
+    lon::Vector
+end
+
+function find_inside(data::pos_data, x0, x1, y1, y2) # Find points inside region e.g: El Nino Basin
+    
+    # I think this works
+    x0 < 0 ? x0 += 360 : nothing
+    x1 < 0 ? x1 += 360 : nothing
+    y0 < 0 ? y0 += 360 : nothing
+    x1 < 0 ? y1 += 360 : nothing
+    
+    i0 = findfirst(
+        x -> x > x0,
+        data.lon
+        )
+    
+    i1 = findfirst(
+        x -> x < x1,
+        data.lon
+    )
+
+    j0 = findfirst(
+        y -> y > y1,
+        data.lat
+    )
+    
+    j1 = findfirst(
+        y -> y < y2,
+        data.lat
+    )
+
+    return pos_data(
+        data.data[i0:i1, j0:j1],
+        data.lat[i0:i1],
+        data.lon[j0:j1]
+    )
+end

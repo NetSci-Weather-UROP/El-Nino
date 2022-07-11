e68d3e35cfd3f49127f6b478bf38f5462f252168
Abhinav Praveen <abhinav@praveen.org.uk>
2022-07-11 11:52:29 +0100

    Fix Typos.

--- nino.jl
+++ nino.jl
@@ -18,18 +18,18 @@ function read_air_data(year)
     return h5open("air.sig995/air.sig995.$year.nc")
 end
 
-function find_inside_indeces(lat, lon, x0, x1, y1, y2) # Find indeces for points inside region e.g: El Nino Basin
+function find_inside_indeces(lat, lon, x0, x1, y0, y1) # Find indeces for points inside region e.g: El Nino Basin
     
     # I think this works
     x0 < 0 ? x0 += 360 : nothing
     x1 < 0 ? x1 += 360 : nothing
     y0 < 0 ? y0 += 360 : nothing
-    x1 < 0 ? y1 += 360 : nothing
+    y1 < 0 ? y1 += 360 : nothing
     
     i0 = findfirst(x -> x > x0, lon)
     i1 = findfirst(x -> x < x1, lon)
-    j0 = findfirst(y -> y > y1, lat)
-    j1 = findfirst(y -> y < y2, lat)
+    j0 = findfirst(y -> y > y0, lat)
+    j1 = findfirst(y -> y < y1, lat)
 
     return i0:i1, j0:j1
 end

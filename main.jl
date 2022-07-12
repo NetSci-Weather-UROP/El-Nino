101a721af59166514a209371dcb19c5570a26c6a
Abhinav Praveen <abhinav@praveen.org.uk>
2022-07-12 16:23:22 +0100

    Pass variables to @write.

--- main.jl
+++ main.jl
@@ -20,17 +20,25 @@ function main()
     window = get_period(data, 11, 1) 
     C, i_point_list, e_point_list = c_i_j(window, enb_is, enb_js)
 	println(C[1:100])
+	
+	i_point_list = hcat(first.(i_point_list),last.(i_point_list))
+	e_point_list = hcat(first.(e_point_list),last.(e_point_list))
+
+	enb_is = Array(enb_is)
+	enb_js = Array(enb_js)
+
+	years = Array(years)
 
     OurNino.HDF5.h5open("generated_data.h5", "w") do file
         OurNino.HDF5.@write file C
-		OurNino.HDF5.@write file hcat(first.(i_point_list),last.(i_point_list))
-		OurNino.HDF5.@write file hcat(first.(e_point_list),last.(e_point_list))
-		OurNino.HDF5.@write file Array(enb_is)
-		OurNino.HDF5.@write file Array(enb_js)
+		OurNino.HDF5.@write file i_point_list
+		OurNino.HDF5.@write file e_point_list
+		OurNino.HDF5.@write file enb_is
+		OurNino.HDF5.@write file enb_js
         OurNino.HDF5.@write file anomaly_data
         OurNino.HDF5.@write file lat
         OurNino.HDF5.@write file lon
-		OurNino.HDF5.@write file Array(years)
+		OurNino.HDF5.@write file years
     end
 
     return 0

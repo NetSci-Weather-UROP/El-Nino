7e0ef351315d7a2462fde98187a86b910f2cac65
Abhinav Praveen <abhinav@praveen.org.uk>
2022-07-12 06:50:11 +0100

    C_i_j

--- nino.jl
+++ nino.jl
@@ -114,6 +114,24 @@ function c_i_j(data, is, js)
     size_A = size(data)
     interior_points = length(is)*length(js)
     exterior_points = size_A[1] * size_A[2] - interior_points
-    C = Array{Float32}(undef, interior_points, exterior_points)
+    C = Array{Array{Float32}(151)}(undef, interior_points, exterior_points)
+
+    i_point_list = [(i,j) for i in is for j in js]
+    e_point_list = [(i,j) for i in 1:size_A[1] for j in 1:size_A[2]]
+    e_point_list = setdiff(e_point_list, i_point_list)
+
+    for i in 1:length(i_point_list)
+        for j in 1:length(e_point_list)
+            x1 = i_point_list[i]
+            x2 = e_point_list[j]
+            C[i,j] = [
+                cross_correlation(
+                    data[x1[1], x1[2], 200:(200+364)],
+                    data[x2[1], x2[2], d:(d+364)]
+                ) for d in 200:-1:50
+                ]
+        end
+    end
+    return C, i_point_list, e_point_list
 end
 end

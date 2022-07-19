2a6e94120ffc29d43a034bfa792f8dc13badd228
Abhinav Praveen <abhinav@praveen.org.uk>
2022-07-19 04:05:41 +0100

    Use CUDA.

--- main.jl
+++ main.jl
@@ -1,16 +1,18 @@
 #!/usr/bin/env julia
 
 using Pkg
-Pkg.add.(["HDF5","StatsBase","LoopVectorization"])
+Pkg.add.(["HDF5","StatsBase","LoopVectorization", "CUDA"])
 
 include("./nino.jl")
-using .OurNino
+using .OurNino, CUDA
 
 function main()
     years=1977:1987
     data, lat, lon = get_data(; years=years)
     anomaly_data = get_anomaly(data; years=years)
-	display(anomaly_data[1:100])
+
+    synchronize()
+    anomaly_array = Array(anomaly_data)
 
     enb_is, enb_js = find_inside_indeces(lat, lon, -170, -120, -5, 5)
 	println()
@@ -19,23 +21,25 @@ function main()
 
     window = get_period(data, 6, 1) 
     C, i_point_list, e_point_list = c_i_j(window, enb_is, enb_js)
-	println(C[1:100])
-	
-	i_point_list = hcat(first.(i_point_list),last.(i_point_list))
-	e_point_list = hcat(first.(e_point_list),last.(e_point_list))
 
+    synchronize()
+    C_array = Array(C)
+	
+	i_point_list = Array(i_point_list)
+	e_point_list = Array(e_point_list)
+    
 	enb_is = Array(enb_is)
 	enb_js = Array(enb_js)
 
 	years = Array(years)
 
     OurNino.HDF5.h5open("generated_data.h5", "w") do file
-        OurNino.HDF5.@write file C
+        OurNino.HDF5.@write file C_array
 		OurNino.HDF5.@write file i_point_list
 		OurNino.HDF5.@write file e_point_list
 		OurNino.HDF5.@write file enb_is
 		OurNino.HDF5.@write file enb_js
-        OurNino.HDF5.@write file anomaly_data
+        OurNino.HDF5.@write file anomaly_array
         OurNino.HDF5.@write file lat
         OurNino.HDF5.@write file lon
 		OurNino.HDF5.@write file years
--- nino.jl
+++ nino.jl
@@ -4,7 +4,7 @@ module OurNino
 
 export get_data, get_anomaly, find_inside_indeces, get_period, c_i_j
 
-using HDF5, Dates, StatsBase, LoopVectorization
+using HDF5, Dates, StatsBase, LoopVectorization, CUDA
 
 """
 Convert NCEP dates to the actual date.
@@ -64,7 +64,7 @@ function mean_air_data(A)
     @inbounds (for i in 1:size_A[1]
         for j in 1:size_A[2]
             for k in 1:365
-                out[i,j,k]  = mean(findmissing.(A[i,j,(4k-3):(4k)]))
+                out[i,j,k] = mean(filter(x->x>0,A[i,j,(4k-3):(4k)]))
             end
         end
     end)
@@ -84,33 +84,63 @@ function get_data(;years=1948:2021)
         data[:,:,:,i] .= mean_air_data(read(A["air"])) # Discard Leap Years (:
         close(A)
     end
-    return data, lat, lon
+    return CuArray(data), lat, lon
 end
 
-function get_anomaly(data; years=1948:2021, radial_period=4, scale=true)
-    # This is a bit silly again. We can do this on a GPU for a speedup though.
-    size_A = size(data)
+function remove_anomaly!(out, data, s_data, l_years, radial_period)
+    ti=threadIdx()
+    x=ti.x
+    y=ti.y
+    T=ti.z
 
-    out = Array{Float32}(undef, size_A[1], size_A[2], 365, length(years))
-    @inbounds (for i in 1:length(years)
-        local_period = intersect(1:length(years), (i-radial_period):(i+radial_period))
-        t = Array{Task}(undef,size_A[1])
-        for j in 1:size_A[1]
-            t[j] = Threads.@spawn for k in 1:size_A[2]
-                for l in 1:365
-                    out[j,k,l,i] = data[j,k,l,i] - mean(filter(x->!isnan(x),data[j,k,l,local_period]))
+    z=blockIdx().x
+    zm = blockDim().x
+
+    tm = gridDim()
+    xm = tm.x
+    ym = tm.y
+    Tm = tm.z
+    
+    @inbounds(for l in z:zm:l_years
+        ll=max(1,l-radial_period)
+        ul=min(l_years,l+radial_period)
+        n=ul-ll+1
+            for k in T:Tm:365
+                for j in y:ym:s_data[2]
+                    for i in x:xm:s_data[1]
+                    out[i,j,k,l] = data[i,j,k,l] - sum(@view(data[x,y,z,ll:ul]))/n
                 end
             end
         end
-        wait.(t)
     end)
+    return
+end
+
+function get_anomaly(data; years=1948:2021, radial_period=4, scale=true)
+    size_A = size(data)
+
+    out = CuArray{Float32}(undef, size_A[1], size_A[2], 365, length(years))
+    
+    kernel = @cuda launch=false remove_anomaly!(out, data, size_A, length(years), radial_period)
+    config = launch_configuration(kernel.fun)
+
+    t1 = min(size_A[1],config[2])
+    t2 = min(fld(config[2],t1),size_A[2])
+    t3 = min(fld(config[2],t1*t2),length(years))
+    t4 = min(365,config[1])
+
+    println(config)
+    println(size_A)
+    println(t1, " ", t2, " ", t3, " ", t4)
+
+    kernel(out, data, size_A, length(years), radial_period; threads=(t1,t2,t3), blocks=t4)
     return out
 end
 
 function get_period(data, y, d; days=715) #715 = 150+200+365
     size_A = size(data)
-    local_data = Array{Float32}(undef, days, size_A[1], size_A[2]) 
-    local_data[1:length(d:365), :, :] .= permutedims(data[:,:,d:365,y],(3,1,2))
+    local_data = CuArray{Float32}(undef, days, size_A[1], size_A[2]) 
+    local_data[1:length(d:365), :, :] .= permutedims(@view(data[:,:,d:365,y]),(3,1,2))
     
     filled_count = length(d:365)
     while filled_count < days
@@ -118,38 +148,79 @@ function get_period(data, y, d; days=715) #715 = 150+200+365
         next_index = days - filled_count
         next_index > 365 ? next_index = 365 : nothing
 
-        local_data[(filled_count+1):(filled_count+next_index), :, :] .= permutedims(data[:,:,1:next_index,y],(3,1,2))
+        local_data[(filled_count+1):(filled_count+next_index), :, :] .= permutedims(@view(data[:,:,1:next_index,y]),(3,1,2))
         filled_count += next_index
     end
     return local_data
+end
+
+function xsq(x)
+    return x^2
+end
+
+function c_i_j!(data, C, i_point_list, e_point_list, interior_points, exterior_points, lags, l_lags)
+    ti=threadIdx()
+    x=ti.x
+    y=ti.y
+
+    z=blockIdx().x
+    zm = blockDim().x
+
+    tm = gridDim()
+    xm = tm.x
+    ym = tm.y
+
+    @inbounds(for j in y:ym:exterior_points
+        x2 = @view(e_point_list[j, :])
+        for i in x:xm:interior_points
+            x1 = @view(i_point_list[i, :])
+            for d in z:zm:l_lags
+                dotmean = 0f0
+                for day in 0:364
+                    dotmean += data[(200+day), x1[1], x1[2]] * data[lags[d]+day, x2[1], x2[2]]
+                end
+                dotmean /= 365
+                mean1 = sum(@view(data[200:(200+364), x1[1], x1[2]]))/365
+                mean2 = sum(@view(data[lags[d]:(lags[d]+364), x2[1], x2[2]]))/365
+                meansq1 = sum(xsq,@view(data[200:(200+364), x1[1], x1[2]]))/365
+                meansq2 = 0f0
+                for day in 0:364
+                    meansq2 += data[lags[d]+day,x2[1],x2[2]]^2
+                end
+                meansq2 /= 365 
+                std1 = sqrt(meansq1 - mean1^2)
+                std2 = sqrt(meansq2 - mean2^2)
+                
+                C[i,j,d] = (dotmean - mean1 * mean2) / std1 / std2
+            end
+        end
+    end)
+    return
 end
 
 function c_i_j(data, is, js; lags=50:350)
     size_A = size(data)
     interior_points = length(is)*length(js)
     exterior_points = size_A[2] * size_A[3] - interior_points
-    C = Array{Float32}(undef, interior_points, exterior_points, length(lags))
+    C = CuArray{Float32}(undef, interior_points, exterior_points, length(lags))
 
     i_point_list = [(i,j) for i in is for j in js]
     e_point_list = [(i,j) for i in 1:size_A[2] for j in 1:size_A[3]]
     e_point_list = setdiff(e_point_list, i_point_list)
+    i_point_list = CuArray(hcat(first.(i_point_list),last.(i_point_list)))
+	e_point_list = CuArray(hcat(first.(e_point_list),last.(e_point_list)))
 
-    t = Array{Task}(undef,interior_points*exterior_points)
-    c=0
-    @inbounds (for j in 1:length(e_point_list)
-        for i in 1:length(i_point_list)
-            c+=1
-            x1 = i_point_list[i]
-            x2 = e_point_list[j]
-            t[c] = Threads.@spawn for d in lags .- (minimum(lags)-1)
-                C[i,j, d] = cor(
-                    @view(data[200:(200+364), x1[1], x1[2]]),
-                    @view(data[lags[d]:(lags[d]+364), x2[1], x2[2]])
-                )
-            end
-        end
-    end)
-    wait.(t)
+    kernel = @cuda launch=false c_i_j!(data, C, i_point_list, e_point_list, interior_points, exterior_points, lags, length(lags))
+    config = launch_configuration(kernel.fun)
+
+    t1 = min(interior_points,config[2])
+    t2 = min(fld(config[2],t1),exterior_points)
+    t4 = min(length(lags),config[1])
+
+    println(config)
+    println(t1, " ", t2, " ", t4)
+    
+    kernel(data, C, i_point_list, e_point_list, interior_points, exterior_points, lags, length(lags); threads=(t1,t2), blocks=t4)
     return C, i_point_list, e_point_list
 end
 end

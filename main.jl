00281111a237dd08a1ae768a24998f5db42b2f33
Abhinav Praveen <abhinav@praveen.org.uk>
2022-07-28 07:53:57 +0100

    Use environment variables to select implementation.

--- main.jl
+++ main.jl
@@ -6,10 +6,28 @@ Pkg.add.(["HDF5","StatsBase","LoopVectorization"])
 include("./nino.jl")
 using .OurNino
 
+if haskey(ENV, "ANOM_IMPL")
+	const anom_impl = ENV["ANOM_IMPL"]
+else
+	const anom_impl = "1"
+end
+
+if haskey(ENV, "C_IMPL")
+	const anom_impl = ENV["C_IMPL"]
+else
+	const anom_impl = "1"
+end
+
 function main()
     years=1977:1987
     data, lat, lon = get_data(; years=years)
-    anomaly_data = get_anomaly(data; years=years)
+
+	if anom_impl == "1"
+    	anomaly_data = get_anomaly(data; years=years)
+	elseif anom_impl == "2"
+		anomaly_data = get_anomaly2(data; years=years)
+	end
+
 	display(anomaly_data[1:100])
 
     enb_is, enb_js = find_inside_indeces(lat, lon, -170, -120, -5, 5)
@@ -18,7 +36,15 @@ function main()
     println(enb_js)
 
     window = get_period(anomaly_data, 6, 1) 
-    C, i_point_list, e_point_list = c_i_j(window, enb_is, enb_js)
+
+	if anom_impl == "1"
+    	C, i_point_list, e_point_list = c_i_j(window, enb_is, enb_js)
+	elseif anom_impl == "2"
+    	C, i_point_list, e_point_list = c_i_j2(window, enb_is, enb_js)
+	elseif anom_impl == "3"
+    	C, i_point_list, e_point_list = c_i_j3(window, enb_is, enb_js)
+	end
+
 	println(C[1:100])
 	
 	i_point_list = hcat(first.(i_point_list),last.(i_point_list))

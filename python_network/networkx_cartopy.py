fb3c203c2bfc907f7809a6ca3782b12cb075c8ba
Lunzhi Shi <ls1420@ic.ac.uk>
2022-08-06 00:31:09 +0100

    Implement naive test model to combine networkx and cartopy with lat and lon

--- python_network/networkx_cartopy.py
+++ python_network/networkx_cartopy.py
@@ -4,6 +4,21 @@ import cartopy.crs as ccrs
 from cartopy.io import shapereader as shpreader
 import matplotlib.pyplot as plt
 
+# testing
+edges = np.array([['A', 'B'],
+                 ['B', 'C'],
+                 ['C', 'D'],
+                 ['C', 'A']])
+pos = {
+       'A': [150, 5], 
+       'B': [110, 15], 
+       'C': [140, -25], 
+       'D': [160, -30]
+}
+
+G = nx.from_edgelist(edges)
+sg = next(G.subgraph(c) for c in nx.connected_components(G))
+
 # draw background map
 crs = ccrs.PlateCarree(central_longitude = 178.75)
 fig, ax = plt.subplots(
@@ -12,4 +27,11 @@ fig, ax = plt.subplots(
 ax.coastlines()
 ax.set_global()
 
+nx.draw_networkx(sg, ax=ax,
+                 font_size=16,
+                 alpha=1,
+                 width=2,
+                 pos=pos,
+                 cmap=plt.cm.autumn)
+                 
 plt.show()
